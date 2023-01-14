9fac3f973fe81cf7a00eab0c63303607c3bd960a
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-01-15 03:34:52 +0530

    Some relative locators added

--- SeleniumTrainingMaven/src/test/java/ui/RelativeLocator.java
+++ SeleniumTrainingMaven/src/test/java/ui/RelativeLocator.java
@@ -1,7 +1,10 @@
 package ui;
 
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,7 +14,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class RelativeLocator {
     public static WebDriver driver;
-    public static String browser = "Edge";
+    public static String browser = "Chrome";
     public static void main(String[] args){
         if(browser == "Chrome"){
             WebDriverManager.chromedriver().setup();
@@ -27,5 +30,14 @@ public class RelativeLocator {
             WebDriverManager.edgedriver().setup();
             driver = new EdgeDriver();
         }
+
+        driver.get("http://www.saucedemo.com/");
+        WebElement password_field = driver.findElement(By.id("password"));
+        driver.findElement(with(By.tagName("input")).above(password_field)).sendKeys("standard_user");
+
+        
+        WebElement username_field = driver.findElement(By.id("user-name"));
+        driver.findElement(with(By.tagName("input")).below(username_field)).sendKeys("secret_sauce");
+        driver.close();
     }
 }
