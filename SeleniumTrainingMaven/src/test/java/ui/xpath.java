42734e56f8e645c0ae4a34c9c34d62ef8b891c2a
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-01-16 03:23:41 +0530

    started xpath

---
+++ SeleniumTrainingMaven/src/test/java/ui/xpath.java
@@ -0,0 +1,37 @@
+package ui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class xpath {
+
+    public static WebDriver driver;
+    public static String browser = "Chrome";
+    public static void main(String[] args){
+        if(browser == "Chrome"){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+
+        else if(browser == "Firefox"){
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+
+        else if(browser == "Edge"){
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
+        
+        driver.get("http://www.saucedemo.com/");
+        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("Testing");
+        // driver.close();
+
+    }
+}
