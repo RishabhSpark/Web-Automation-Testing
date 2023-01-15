e043939ff02abe82c4019310066e32efad66176c
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-01-16 03:44:21 +0530

    Relative and absolute xpath

--- SeleniumTrainingMaven/src/test/java/ui/xpath.java
+++ SeleniumTrainingMaven/src/test/java/ui/xpath.java
@@ -30,7 +30,13 @@ public class xpath {
         }
         
         driver.get("http://www.saucedemo.com/");
-        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("Testing");
+
+        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("Testing"); // Relative xpath
+        
+        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div/div/form/div[2]/input")).sendKeys("Testing");// Absolute xpath(Written by me)
+        driver.findElement(By.xpath("/html/body/div/div/div[2]/div[1]/div[1]/div/form/div[2]/input")).sendKeys("Testing");// Absolute xpath(Copied from inspect element)
+        
+        
         // driver.close();
 
     }
