bcb90200cc7743d5f7683aa1f8e624e2d60c3019
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-01-17 22:48:05 +0530

    XPath with indices and they start with 1(why even :/)

---
+++ References/xpath_indices.java
@@ -0,0 +1,26 @@
+/*
+
+Used to find tags with indices
+**IMPORTANT** -> Indices starts with 1(YEP not 0)
+FORMAT -> (//tagname[@Attribute='Value'])[index]
+
+*/
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class xpath_indices {        
+        driver.get("http://www.saucedemo.com/");
+        driver.findElement(By.xpath("(//input)[1]")).sendKeys("standard_user");
+        driver.findElement(By.xpath("(//input)[2]")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("(//input)[3]")).click();
+        driver.close();
+
+    }
+}
