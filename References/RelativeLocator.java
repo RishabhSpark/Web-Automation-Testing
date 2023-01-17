a615ce08003576e69dc8abe249dc05b2562fe9f8
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-01-17 18:11:23 +0530

    References for relative locators

--- References/RelativeLocator.java
+++ References/RelativeLocator.java
@@ -12,36 +12,8 @@ Chaining relative locators  You can also chain locators if needed. Sometimes the
 
 import static org.openqa.selenium.support.locators.RelativeLocator.with;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-
 public class RelativeLocator {
-    public static WebDriver driver;
-    public static String browser = "Chrome";
     public static void main(String[] args){
-        if(browser == "Chrome"){
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-        }
-
-        else if(browser == "Firefox"){
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
-        }
-
-        else if(browser == "Edge"){
-            WebDriverManager.edgedriver().setup();
-            driver = new EdgeDriver();
-        }
-
-        driver.get("http://www.saucedemo.com/");
 
         // HERE ARE THE EXAMPLES.
         WebElement password_field = driver.findElement(By.id("password"));
