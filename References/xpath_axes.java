da63e51880e158669e21ed2e3437a99d79af7212
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-01-18 17:18:28 +0530

    Yo it has family and stuff

--- References/xpath_axes.java
+++ References/xpath_axes.java
@@ -1,10 +1,13 @@
 /*
 
-AXES            USE                                     FORMAT
-self            Indicates the node itself               //tagname[@Attribute='Value']//self::tagname
-parent          Indicates the parent node               //tagname[@Attribute='Value']//parent::tagname    
-child           Indicates the child node                //tagname[@Attribute='Value']//child::tagname 
-
+AXES                USE                                                     FORMAT
+self                Indicates the node itself                               //tagname[@Attribute='Value']//self::tagname
+parent              Indicates the parent node                               //tagname[@Attribute='Value']//parent::tagname    
+child               Indicates the child node                                //tagname[@Attribute='Value']//child::tagname 
+descendant          Selects all descendant(child, gandchild, etc.)          //tagname[@Attribute='Value']//descendant::tagname
+                    of current node          
+descendant-or-self  Selects current node + all descendants                  //tagname[@Attribute='Value']//descendant-or-self::tagname
+                    (if tagname of descendant and self are same)
 */
 
 import org.openqa.selenium.By;
@@ -15,8 +18,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-
 public class xpath_axes {
+    public static void main(String[] args){
         driver.get("https://accounts.lambdatest.com/register");
         
         driver.findElement(By.xpath("//input[@id='name']//self::input")).sendKeys("Name???"); //self axes
@@ -24,7 +27,27 @@ public class xpath_axes {
         driver.findElement(By.xpath("//option[@data-countrycode='AF']//parent::select")).click(); //parent axes
         driver.findElement(By.xpath("//select[@id='country_code']//parent::div//child::input")).sendKeys("234567825"); //parent and child axes together
         
-        // driver.close();
 
+        //      -----------------------------------------XPATH DESCENDANT(or-self) + getAttribute with List-----------------------------------------
+        // THIS FOR ONLY ONE
+        // WebElement attribute_select = driver.findElement(By.xpath("//select[@id='country_code']//descendant::option"));
+        // String label_attribute = attribute_select.getAttribute("label");
+        // String value_attribute = attribute_select.getAttribute("value");
+        // System.out.println(label_attribute + " " + value_attribute);
+
+        // THIS FOR ALL, STORING IN LIST AND THEN ITERATING OVER ALL. USED **findElements** (plural) HERE.
+        List<WebElement> attribute_select = driver.findElements(By.xpath("//select[@id='country_code']//descendant::option"));
+        for(WebElement element : attribute_select){   
+            String label_attribute = element.getAttribute("label");
+            String value_attribute = element.getAttribute("value");
+            System.out.println(label_attribute + " " + value_attribute+"\n");
+        }
+
+        List<WebElement> all_div_descendant_or_self = driver.findElements(By.xpath("//div[@class='clearfix registeraArea signUpWithEmail']//descendant-or-self::div"));
+        for(WebElement element : all_div_descendant_or_self){   
+            String class_attribute = element.getAttribute("class");
+            System.out.println(class_attribute+"\n");
+        }       
+        // driver.close();
     }
 }
