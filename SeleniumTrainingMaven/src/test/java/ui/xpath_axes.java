58f79424241874fa69f4bc9e2947ff56cdb1acdc
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-01-18 17:17:20 +0530

    descendant and descendant or self

--- SeleniumTrainingMaven/src/test/java/ui/xpath_axes.java
+++ SeleniumTrainingMaven/src/test/java/ui/xpath_axes.java
@@ -1,7 +1,10 @@
 package ui;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -35,6 +38,29 @@ public class xpath_axes {
         driver.findElement(By.xpath("//select[@id='country_code']//child::option[@data-countrycode='AS']")).click(); //child axes
         driver.findElement(By.xpath("//option[@data-countrycode='AF']//parent::select")).click(); //parent axes
         driver.findElement(By.xpath("//select[@id='country_code']//parent::div//child::input")).sendKeys("234567825"); //parent and child axes together
+
+        
+        
+//      -----------------------------------------XPATH DESCENDANT(or-self) + getAttribute with List-----------------------------------------
+        // THIS FOR ONLY ONE
+        // WebElement attribute_select = driver.findElement(By.xpath("//select[@id='country_code']//descendant::option"));
+        // String label_attribute = attribute_select.getAttribute("label");
+        // String value_attribute = attribute_select.getAttribute("value");
+        // System.out.println(label_attribute + " " + value_attribute);
+
+        // THIS FOR ALL, STORING IN LIST AND THEN ITERATING OVER ALL. USED **findElements** (plural) HERE.
+        List<WebElement> attribute_select = driver.findElements(By.xpath("//select[@id='country_code']//descendant::option"));
+        for(WebElement element : attribute_select){   
+            String label_attribute = element.getAttribute("label");
+            String value_attribute = element.getAttribute("value");
+            System.out.println(label_attribute + " " + value_attribute+"\n");
+        }
+
+        List<WebElement> all_div_descendant_or_self = driver.findElements(By.xpath("//div[@class='clearfix registeraArea signUpWithEmail']//descendant-or-self::div"));
+        for(WebElement element : all_div_descendant_or_self){   
+            String class_attribute = element.getAttribute("class");
+            System.out.println(class_attribute+"\n");
+        }       
         
         // driver.close();
 
