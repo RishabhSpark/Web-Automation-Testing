637f01b6b72aa36f0d6199bcf7cc3ab3660bde8c
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-01-18 17:07:40 +0530

    Everything about Locators huehehehe

--- References/Locators.java
+++ References/Locators.java
@@ -21,7 +21,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Locators {
-        
+    public static void main(String[] args){
         // driver.get("http://www.saucedemo.com/");
         driver.get("https://www.selenium.dev/documentation/webdriver/elements/locators/");
         
