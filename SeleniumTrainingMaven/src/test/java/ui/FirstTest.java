8f63a45d645669e55221a017bc816a0845c61779
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-01-18 22:09:33 +0530

    edit

--- SeleniumTrainingMaven/src/test/java/ui/FirstTest.java
+++ SeleniumTrainingMaven/src/test/java/ui/FirstTest.java
@@ -6,7 +6,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class FirstTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 //		System.setProperty("webdriver.chrome.driver", "C:\\Users\\asus\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriverManager.chromedriver().setup();
 		
