26fcec6b708041352e4bbdabad9a68876a0390ce
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-01-25 18:44:29 +0530

    keyboard actions

--- References/keyboardactions.java
+++ References/keyboardactions.java
@@ -5,36 +5,17 @@ KEYBOARD ACTIONS -> https://www.selenium.dev/documentation/webdriver/actions_api
 KEYS LIST -> https://github.com/SeleniumHQ/selenium/blob/selenium-4.2.0/java/src/org/openqa/selenium/Keys.java#L28
 
 */
-package actions;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class keyboardactions {
-    public static WebDriver driver;
-    public static String browser = "Chrome";
     public static void main(String[] args) {
-        if(browser.equals("Chrome")){
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-        }
-        if(browser.equals("Firefox")){
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
-        }
-        if(browser.equals("Edge")){
-            WebDriverManager.edgedriver().setup();
-            driver = new EdgeDriver();
-        }
-
         driver.get("https://extendsclass.com/text-compare.html");
         driver.manage().window().maximize();
 
@@ -48,12 +29,6 @@ public class keyboardactions {
         WebElement destination = driver.findElement(By.xpath("//div[@id='dropZone2']"));
         action.keyDown(destination, Keys.CONTROL).sendKeys("a").sendKeys(Keys.DELETE).sendKeys("v").build().perform();
 
-
-
-
-        WebElement error_check_element = driver.findElement(By.xpath("//div[@id='nb-diff']"));
-        String error_value = error_check_element.getText();
-        System.out.println(error_value);
-        driver.quit();        
+        // driver.quit();        
     }
 }
