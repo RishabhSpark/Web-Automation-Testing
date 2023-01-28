dde38e7e2d3660e4fbc16f4ae69435e9aa62e03c
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-01-28 21:11:25 +0530

    BeforeTest, AfterTest, BeforeMethod, AfterMethod

--- SeleniumTestNG/src/test/java/ui/FirstTest.java
+++ SeleniumTestNG/src/test/java/ui/FirstTest.java
@@ -5,16 +5,48 @@ We don't need main(psvm) method in TestNG, we usually need class, and methods wi
 
 TestNG runs the tests in Alphabetically. To change that, we need add "priority" attribute to the testcase.
 
-@Test
+@Test                   Marks a class or a method as part of the test.
 priority                The priority for this test method. Lower priorities will be scheduled first.
 description	            The description for this method.
- 
+
+
+@BeforeTest: The annotated method will be run before any test method belonging to the classes inside the <test> tag is run.
+@AfterTest: The annotated method will be run after all the test methods belonging to the classes inside the <test> tag have run.
+
+@BeforeMethod: The annotated method will be run before each test method.
+@AfterMethod: The annotated method will be run after each test method.
+
+
 */
 package ui;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class FirstTest {
+    @BeforeTest
+    public void beforeTest(){
+        System.out.println("Runs before all tests");
+    }
+
+    @AfterTest
+    public void afterTest(){
+        System.out.println("Runs after all tests");
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("Runs before each test");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("Runs after each test");
+    }
+
     @Test(priority = 1, description = "This is the first test")
     public void BTest(){
         System.out.println("Test1");
