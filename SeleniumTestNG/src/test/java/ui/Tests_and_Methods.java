36bb88c863f3fe444c9d4715497c4617f551ab64
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-01-28 21:13:35 +0530

    renamed

--- SeleniumTestNG/src/test/java/ui/Tests_and_Methods.java
+++ SeleniumTestNG/src/test/java/ui/Tests_and_Methods.java
@@ -26,7 +26,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class FirstTest {
+public class Tests_and_Methods {
     @BeforeTest
     public void beforeTest(){
         System.out.println("Runs before all tests");
