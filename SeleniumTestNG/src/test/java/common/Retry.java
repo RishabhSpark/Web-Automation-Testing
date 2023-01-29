5e32616e6be540d3cce46762d2892809ebe0b520
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-01-30 01:20:23 +0530

    Retry added

---
+++ SeleniumTestNG/src/test/java/common/Retry.java
@@ -0,0 +1,17 @@
+package common;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+ 
+public class Retry implements IRetryAnalyzer {
+    private int retryCount = 0;
+    private static final int maxRetryCount = 5;
+ 
+    public boolean retry(ITestResult result) {
+    if (retryCount < maxRetryCount) {
+        retryCount++;
+        return true;
+    }
+    return false;
+    }
+}
