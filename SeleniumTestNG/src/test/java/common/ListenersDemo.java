609cf28dc1926a7f71242e684fc03d02175ea148
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-01-30 01:45:29 +0530

    Retry Listener

--- SeleniumTestNG/src/test/java/common/ListenersDemo.java
+++ SeleniumTestNG/src/test/java/common/ListenersDemo.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ListenersDemo extends baseTest{
-    @Test
+    @Test(retryAnalyzer = common.Retry.class)
     public void launchApp(){
         driver.get("https://ebay.com");
         Assert.assertTrue(false);
